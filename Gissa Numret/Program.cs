3d6781cd296d17575069fb95fcd5cba75715b096
Ejpatolitej <niklassonjack@gmail.com>
2021-09-27 10:34:27 +0200

    Add project files.

---
+++ Gissa Numret/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace Gissa_Numret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool keepRunning = true;
+            while (keepRunning == true)
+            {
+                try
+                {
+                    Console.Clear();
+                    Console.WriteLine("\tHejsan! Välkommen till mitt \"gissa numret\" spel!");
+                    Console.WriteLine("\tVilken svårighetsgrad vill du köra på? (1-1000)");
+                    Console.Write("\t");
+                    int difficulty = Int32.Parse(Console.ReadLine());
+                    checkNumber(difficulty);
+                    Console.Clear();
+
+                    Console.WriteLine("\tVill du spela igen? Ja / Nej");
+                    Console.Write("\t");
+                    string keepPlaying = Console.ReadLine();
+                    if (keepPlaying.ToUpper() == "JA")
+                    {
+
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("\tTack för att du spelade!");
+                        keepRunning = false;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Hoppsan! Nu var det något som gick fel!");
+                }
+            }
+        }
+        public static void checkNumber(int difficulty)
+        {
+            try
+            {
+                string[] tooHigh = { "Tyvärr, för högt!", "Attans, det var för högt!", "Nej, nej. För högt!", "Aj då, gissa på ett lägre tal!", "Nix pix fågelkrix! Nu vart det för högt!" };
+                string[] tooLow = { "Tyvärr, för lågt!", "Attans, det var för lågt!", "Nej, nej. För lågt!", "Aj då, gissa på ett högre tal!", "Nix pix fågelkrix! Nu vart det för lågt!" };
+
+                Random rnd = new Random();
+                int rndNmbr = rnd.Next(1, difficulty);
+                int chances = 5 + difficulty / 20;
+
+                for (int i = 0; i < chances; i++)
+                {
+                    Console.WriteLine("\tDu har " + (chances - i) + " chanser på dig");
+                    Console.Write("\tGissa på ett nummer: ");
+                    int guess = Int32.Parse(Console.ReadLine());
+
+                    if (guess == rndNmbr)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("\tGrattis! Du gissade rätt!");
+                        break;
+                    }
+                    else if (guess > rndNmbr)
+                    {
+                        if (guess <= rndNmbr + 2)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("\tNu bränns det!");
+                        }
+                        else if (guess <= rndNmbr + 5)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("\tNu är det nära! Lite lägre!");
+                        }
+                        else if (guess > rndNmbr)
+                        {
+                            int rndTooHigh = rnd.Next(tooHigh.Length);
+                            Console.Clear();
+                            Console.WriteLine($"\t{tooHigh[rndTooHigh]}");
+                        }
+                    }
+                    else if (guess < rndNmbr)
+                    {
+                        if (guess >= rndNmbr - 2)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("\tNu bränns det!");
+                        }
+                        else if (guess >= rndNmbr - 5)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("\tNu är det nära! Lite högre!");
+                        }
+                        else if (guess < rndNmbr)
+                        {
+                            int rndTooLow = rnd.Next(tooLow.Length);
+                            Console.Clear();
+                            Console.WriteLine($"\t{tooLow[rndTooLow]}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nu blev det fel!");
+                    }
+                }
+                Console.WriteLine("\tRätt nummer var " + rndNmbr + "!");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            catch (Exception)
+            {
+                Console.Clear();
+                Console.WriteLine("\tHoppsan! Nu var det något som blev fel!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
